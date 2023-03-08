f7550cacfd1743327e49201b3eb73615c4b4e935
Lou Bernabeu <bernabeu.lou@gmail.com>
2023-03-08 15:32:04 +0100

    made sure I could get normal bands out of MPB. All good

--- tests/modesolver-fabry-dbr.py
+++ tests/modesolver-fabry-dbr.py
@@ -3,14 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import e, hbar
 from meep.mpb import ModeSolver
+from meep import mpb
 
 
 eps1 = 1
 eps2 = 3.45
 
-N = 1
+N = 2
 
-pml_thickness = 17.0
+pml_thickness = 1.0
+
+cavity_transverse_extent = 0.5
+
+## MPB VARIABLES
+
+RESOLUTION = 21
+NUM_BANDS = 2
+INTERP_POINTS = 31
 
 
 ### FUNCTIONS
@@ -41,19 +50,19 @@ def gen_fabry_geometry(N, eps1, eps2):
 
         bilayer_center = half_cavity_width + i*grating_periodicity/2
 
-        eps2_layer_center = mp.Vector3(0, 0, bilayer_center + 0.5*(lay_th_2 - grating_periodicity))
-        eps1_layer_center = mp.Vector3(0, 0, bilayer_center + 0.5*(grating_periodicity - lay_th_1))
+        eps2_layer_center = mp.Vector3(bilayer_center + 0.5*(lay_th_2 - grating_periodicity), 0, 0)
+        eps1_layer_center = mp.Vector3(bilayer_center + 0.5*(grating_periodicity - lay_th_1), 0, 0)
 
-        geometry += [mp.Block(mp.Vector3(0, 0, lay_th_2), # right eps2 layer
+        geometry += [mp.Block(mp.Vector3(lay_th_2, cavity_transverse_extent, 0), # right eps2 layer
                               center=eps2_layer_center,
                               material=mp.Medium(epsilon=eps2)),
-                    mp.Block(mp.Vector3(0, 0, lay_th_2),  # left eps2 layer
+                    mp.Block(mp.Vector3(lay_th_2, cavity_transverse_extent, 0),  # left eps2 layer
                               center= mp.Vector3()-eps2_layer_center,
                               material=mp.Medium(epsilon=eps2)),
-                    mp.Block(mp.Vector3(0, 0, lay_th_1),  # right eps1 layer
+                    mp.Block(mp.Vector3(lay_th_1, cavity_transverse_extent, 0),  # right eps1 layer
                               center=eps1_layer_center,
                               material=mp.Medium(epsilon=eps1)),
-                    mp.Block(mp.Vector3(0, 0, lay_th_1),  # left eps1 layer
+                    mp.Block(mp.Vector3(lay_th_1, cavity_transverse_extent, 0),  # left eps1 layer
                               center= mp.Vector3()-eps1_layer_center,
                               material=mp.Medium(epsilon=eps1))]
 
@@ -63,54 +72,58 @@ def gen_fabry_geometry(N, eps1, eps2):
 
 geometry, sim_half_width, omega, half_cavity_width = gen_fabry_geometry(N, eps1, eps2)
 
-resolution = 21
 
 cell = mp.Vector3(2*sim_half_width)
-#vol = mp.Volume(2*sim_half_width)
 
+geometry_lattice = mp.Lattice(size=mp.Vector3(sim_half_width, cavity_transverse_extent))
 
-NUM_BANDS = 2
-geometry_lattice = mp.Lattice(size=cell)
+k_points = [mp.Vector3(),          # Gamma
+            mp.Vector3(0.5)]          # Gamma
 
-k_points = [mp.Vector3(0, 0,0), mp.Vector3(0, 0, 10*1/(2*omega))]
-k_points = mp.interpolate(150, k_points)
+k_points = mp.interpolate(INTERP_POINTS, k_points)
 
-omega_values = np.linspace(0.5*omega, 10*omega, 151)
-k_bands = np.zeros((omega_values.size, NUM_BANDS))
 
 ms = ModeSolver(num_bands=NUM_BANDS,
                 k_points = k_points,
                 geometry=geometry,
                 geometry_lattice=geometry_lattice,
-                resolution=resolution,
-                dimensions = 1)
+                resolution=RESOLUTION)
 
-for i, current_omega in enumerate(omega_values):
-    k_bands[i, :] = ms.find_k(
-        mp.NO_PARITY,
-        omega,
-        1,
-        NUM_BANDS,
-        mp.Vector3(0, 0, 1),
-        1e-3,
-        current_omega * eps2,
-        current_omega*0.1,
-        current_omega*4)
+# for i, current_omega in enumerate(omega_values):
+#     k_bands[i, :] = ms.find_k(
+#         mp.NO_PARITY,
+#         omega,
+#         1,
+#         NUM_BANDS,
+#         mp.Vector3(0, 0, 1),
+#         1e-3,
+#         current_omega * eps2,
+#         current_omega*0.1,
+#         current_omega*4)
 
 
-fig, ax = plt.subplots(1, 2)
-ax1, ax2 = ax
-ax1.set_xlabel(r"$\omega$")
-ax2.set_xlabel(r"$\omega$")
+# fig, ax = plt.subplots(1, 2)
+# ax1, ax2 = ax
+# ax1.set_xlabel(r"$\omega$")
+# ax2.set_xlabel(r"$\omega$")
 
-ax1.set_ylabel(r"$k$")
-ax2.set_xlabel(r"$n$")
+# ax1.set_ylabel(r"$k$")
+# ax2.set_xlabel(r"$n$")
 
-for band in range(NUM_BANDS):
-    ax1.plot(omega_values, k_bands[:, band], label = f"band {band + 1}")
-    ax2.plot(omega_values, k_bands[:, band]/omega_values, label = f"band {band + 1}")
+# for band in range(NUM_BANDS):
+#     ax1.plot(omega_values, k_bands[:, band], label = f"band {band + 1}")
+#     ax2.plot(omega_values, k_bands[:, band]/omega_values, label = f"band {band + 1}")
 
-ax1.legend()
-ax2.legend()
+# ax1.legend()
+# ax2.legend()
+# plt.show()
+
+ms.run_te()
+
+fig, ax = plt.subplots()
+
+s = ms.all_freqs.T
+for i in s :
+    ax.plot(i)
+
 plt.show()
-
